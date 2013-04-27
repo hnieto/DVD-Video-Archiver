10892a8f1ba20e77d3910e5a909cc5a9aee98403
hnieto <hnieto7@gmail.com>
2013-04-27 02:41:16 -0500

    browse buttons populate text boxes with selected directory from dialog

--- dvdArchiver.py
+++ dvdArchiver.py
@@ -12,7 +12,7 @@ import wx
 class Archiver(wx.Frame):
 
     def __init__(self, parent, title):    
-        super(Archiver, self).__init__(parent, title=title, size=(450, 400))
+        super(Archiver, self).__init__(parent, title=title, size=(500, 400))
 
         self.InitUI()
         self.Centre()
@@ -20,82 +20,90 @@ class Archiver(wx.Frame):
 
     def InitUI(self):
       
-        panel = wx.Panel(self)
+        self.panel = wx.Panel(self)
         
-        sizer = wx.GridBagSizer(5, 5)
+        self.gridSizer = wx.GridBagSizer(5, 5)
 
         # header
-        text1 = wx.StaticText(panel, label="DVD Video Archiver")
-        sizer.Add(text1, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=15)
+        self.label1 = wx.StaticText(self.panel, label="DVD Video Archiver")
+        self.gridSizer.Add(self.label1, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, border=15)
 
-        icon = wx.StaticBitmap(panel, bitmap=wx.Bitmap('icon.png'))
-        sizer.Add(icon, pos=(0, 4), flag=wx.TOP|wx.RIGHT|wx.ALIGN_RIGHT, border=5)
+        self.icon = wx.StaticBitmap(self.panel, bitmap=wx.Bitmap('icon.png'))
+        self.gridSizer.Add(self.icon, pos=(0, 4), flag=wx.TOP|wx.RIGHT|wx.ALIGN_RIGHT, border=5)
 
-        line = wx.StaticLine(panel)
-        sizer.Add(line, pos=(1, 0), span=(1, 5), flag=wx.EXPAND|wx.BOTTOM, border=10)
+        self.divider = wx.StaticLine(self.panel)
+        self.gridSizer.Add(self.divider, pos=(1, 0), span=(1, 5), flag=wx.EXPAND|wx.BOTTOM, border=10)
 
         # text boxes
-        text2 = wx.StaticText(panel, label="Output Directory")
-        sizer.Add(text2, pos=(2, 0), flag=wx.LEFT|wx.TOP, border=10)
+        self.label2 = wx.StaticText(self.panel, label="Output Directory")
+        self.gridSizer.Add(self.label2, pos=(2, 0), flag=wx.LEFT|wx.TOP, border=10)
 
-        tc1 = wx.TextCtrl(panel)
-        sizer.Add(tc1, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
+        self.textBox1 = wx.TextCtrl(self.panel)
+        self.gridSizer.Add(self.textBox1, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
         
-        button1 = wx.Button(panel, label="Browse...")
-        sizer.Add(button1, pos=(2, 4), flag=wx.TOP|wx.RIGHT, border=5)
+        self.button1 = wx.Button(self.panel, label="Browse...")
+        self.gridSizer.Add(self.button1, pos=(2, 4), flag=wx.TOP|wx.RIGHT, border=5)
 
-        text3 = wx.StaticText(panel, label="Output File Name")
-        sizer.Add(text3, pos=(3, 0), flag=wx.LEFT|wx.TOP, border=10)
+        self.label3 = wx.StaticText(self.panel, label="Output File Name")
+        self.gridSizer.Add(self.label3, pos=(3, 0), flag=wx.LEFT|wx.TOP, border=10)
 
-        tc2 = wx.TextCtrl(panel)
-        sizer.Add(tc2, pos=(3, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
+        self.textBox2 = wx.TextCtrl(self.panel)
+        self.gridSizer.Add(self.textBox2, pos=(3, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
 
-        text4 = wx.StaticText(panel, label="DVD Directory")
-        sizer.Add(text4, pos=(4, 0), flag=wx.TOP|wx.LEFT, border=10)
+        self.label4 = wx.StaticText(self.panel, label="DVD Directory")
+        self.gridSizer.Add(self.label4, pos=(4, 0), flag=wx.TOP|wx.LEFT, border=10)
         
-        tc3 = wx.TextCtrl(panel)
-        sizer.Add(tc3, pos=(4, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
+        self.textBox3 = wx.TextCtrl(self.panel)
+        self.gridSizer.Add(self.textBox3, pos=(4, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
         
-        button2 = wx.Button(panel, label="Browse...")
-        sizer.Add(button2, pos=(4, 4), flag=wx.TOP|wx.RIGHT, border=5)
+        self.button2 = wx.Button(self.panel, label="Browse...")
+        self.gridSizer.Add(self.button2, pos=(4, 4), flag=wx.TOP|wx.RIGHT, border=5)
 
         # check boxes
-        sb = wx.StaticBox(panel, label="Optional Attributes")
+        self.staticBox = wx.StaticBox(self.panel, label="Optional Attributes")
 
-        boxsizer = wx.StaticBoxSizer(sb, wx.VERTICAL)
-        makeISO = wx.CheckBox(panel, label="Create ISO")
-        makeISO.SetValue(True)
-        makeMKV = wx.CheckBox(panel, label="Create Matroska")
-        makeMP4 = wx.CheckBox(panel, label="Create MP4")
-        makeMP4.SetValue(True)
-        boxsizer.Add(makeISO, flag=wx.LEFT|wx.TOP, border=5)
-        boxsizer.Add(makeMKV, flag=wx.LEFT, border=5)
-        boxsizer.Add(makeMP4,flag=wx.LEFT, border=5)
-        boxsizer.Add((0,10), 0)
-        sizer.Add(boxsizer, pos=(6, 0), span=(1, 5), flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT , border=10)
+        self.boxSizer = wx.StaticBoxSizer(self.staticBox, wx.VERTICAL)
+        self.makeISO = wx.CheckBox(self.panel, label="Create ISO")
+        self.makeISO.SetValue(True)
+        self.makeMKV = wx.CheckBox(self.panel, label="Create Matroska")
+        self.makeMP4 = wx.CheckBox(self.panel, label="Create MP4")
+        self.makeMP4.SetValue(True)
+        self.boxSizer.Add(self.makeISO, flag=wx.LEFT|wx.TOP, border=5)
+        self.boxSizer.Add(self.makeMKV, flag=wx.LEFT, border=5)
+        self.boxSizer.Add(self.makeMP4,flag=wx.LEFT, border=5)
+        self.boxSizer.Add((0,10), 0)
+        self.gridSizer.Add(self.boxSizer, pos=(6, 0), span=(1, 5), flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT , border=10)
 
         # control buttons
-        button3 = wx.Button(panel, label='Help')
-        sizer.Add(button3, pos=(8, 0), flag=wx.LEFT, border=10)
+        self.button3 = wx.Button(self.panel, label='Help')
+        self.gridSizer.Add(self.button3, pos=(8, 0), flag=wx.LEFT, border=10)
 
-        button4 = wx.Button(panel, label="Archive")
-        sizer.Add(button4, pos=(8, 3))
+        self.button4 = wx.Button(self.panel, label="Archive")
+        self.gridSizer.Add(self.button4, pos=(8, 3))
 
-        button5 = wx.Button(panel, label="Cancel")
-        sizer.Add(button5, pos=(8, 4), span=(1, 1), flag=wx.BOTTOM|wx.RIGHT, border=5)
+        self.button5 = wx.Button(self.panel, label="Cancel")
+        self.gridSizer.Add(self.button5, pos=(8, 4), span=(1, 1), flag=wx.BOTTOM|wx.RIGHT, border=5)
 
-        sizer.AddGrowableCol(2)
+        self.gridSizer.AddGrowableCol(2)
         
-        panel.SetSizer(sizer)
+        self.panel.SetSizer(self.gridSizer)
         
         # event handling
-        button1.Bind(wx.EVT_BUTTON, self.openDir)
-        button2.Bind(wx.EVT_BUTTON, self.openDir)
+        self.button1.Bind(wx.EVT_BUTTON, self.get_output_dir)
+        self.button2.Bind(wx.EVT_BUTTON, self.get_dvd_dir)
         
-    def openDir(self, event):
+    def get_output_dir(self, event):
         dlg = wx.DirDialog(self, "", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
         if dlg.ShowModal() == wx.ID_OK:
-            self.SetStatusText('You selected: %s\n' % dlg.GetPath())
+            self.selectedPath = dlg.GetPath()
+            self.textBox1.SetValue(self.selectedPath)
+        dlg.Destroy()
+        
+    def get_dvd_dir(self, event):
+        dlg = wx.DirDialog(self, "", style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.selectedPath = dlg.GetPath()
+            self.textBox3.SetValue(self.selectedPath)
         dlg.Destroy()
 
 
