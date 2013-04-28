6758e1d2ffd13f23e05c292d83ab523ae762bb6a
hnieto <hnieto7@gmail.com>
2013-04-28 15:58:52 -0500

    split About and Help into 2 pop ups. Added text box validator class

--- dvdArchiver.py
+++ dvdArchiver.py
@@ -10,6 +10,8 @@ Note: wxPython requires Python to be run in 32-bit mode. If the computer running
 import sys
 import traceback
 from wx.lib.wordwrap import wordwrap
+import wx.lib.agw.genericmessagedialog as GMD
+from textBoxValidator import textBoxValidator
 import wx
 
 class Archiver(wx.Frame):
@@ -41,7 +43,7 @@ class Archiver(wx.Frame):
         self.label2 = wx.StaticText(self.panel, label="Output Directory")
         self.gridSizer.Add(self.label2, pos=(2, 0), flag=wx.LEFT|wx.TOP, border=10)
 
-        self.textBox1 = wx.TextCtrl(self.panel)
+        self.textBox1 = wx.TextCtrl(self.panel, validator=textBoxValidator())
         self.gridSizer.Add(self.textBox1, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
         
         self.button1 = wx.Button(self.panel, label="Browse...")
@@ -50,13 +52,13 @@ class Archiver(wx.Frame):
         self.label3 = wx.StaticText(self.panel, label="Output File Name")
         self.gridSizer.Add(self.label3, pos=(3, 0), flag=wx.LEFT|wx.TOP, border=10)
 
-        self.textBox2 = wx.TextCtrl(self.panel)
+        self.textBox2 = wx.TextCtrl(self.panel, validator=textBoxValidator())
         self.gridSizer.Add(self.textBox2, pos=(3, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
 
         self.label4 = wx.StaticText(self.panel, label="DVD Directory")
         self.gridSizer.Add(self.label4, pos=(4, 0), flag=wx.TOP|wx.LEFT, border=10)
         
-        self.textBox3 = wx.TextCtrl(self.panel)
+        self.textBox3 = wx.TextCtrl(self.panel, validator=textBoxValidator())
         self.gridSizer.Add(self.textBox3, pos=(4, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND, border=5)
         
         self.button2 = wx.Button(self.panel, label="Browse...")
@@ -79,18 +81,21 @@ class Archiver(wx.Frame):
         
         # logging
         self.log = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE)
-        self.log.AppendText("Program Log")
+        self.log.AppendText("Waiting on user ...")
         self.gridSizer.Add(self.log, pos=(8, 0), span=(9, 5), flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT , border=10)
 
         # control buttons
-        self.button3 = wx.Button(self.panel, label='Help')
-        self.gridSizer.Add(self.button3, pos=(18, 0), flag=wx.LEFT, border=10)
+        self.about = wx.Button(self.panel, label='About')
+        self.gridSizer.Add(self.about, pos=(18, 0), flag=wx.LEFT, border=10)
+        
+        self.help = wx.Button(self.panel, label='Help')
+        self.gridSizer.Add(self.help, pos=(18, 1), flag=wx.LEFT, border=-60)
 
-        self.button4 = wx.Button(self.panel, label="Archive")
-        self.gridSizer.Add(self.button4, pos=(18, 3))
+        self.archive = wx.Button(self.panel, label="Archive")
+        self.gridSizer.Add(self.archive, pos=(18, 3))
 
-        self.button5 = wx.Button(self.panel, label="Cancel")
-        self.gridSizer.Add(self.button5, pos=(18, 4), span=(1, 1), flag=wx.BOTTOM|wx.RIGHT, border=5)
+        self.cancel = wx.Button(self.panel, label="Cancel")
+        self.gridSizer.Add(self.cancel, pos=(18, 4), span=(1, 1), flag=wx.BOTTOM|wx.RIGHT, border=5)
 
         self.gridSizer.AddGrowableCol(2)
         
@@ -99,24 +104,46 @@ class Archiver(wx.Frame):
         # event handling
         self.button1.Bind(wx.EVT_BUTTON, self.get_output_dir)
         self.button2.Bind(wx.EVT_BUTTON, self.get_dvd_dir)
-        self.button3.Bind(wx.EVT_BUTTON, self.open_help)
+        self.about.Bind(wx.EVT_BUTTON, self.open_about)
+        self.help.Bind(wx.EVT_BUTTON, self.open_help)
+        self.archive.Bind(wx.EVT_BUTTON, self.run_app)
+                
+    def run_app(self, event):
+        self.panel.Validate()
         
     def open_help(self, event):
-        info = wx.AboutDialogInfo()
-        info.Name = "DVD Video Archiver"
-        info.Version = "(version 0.0.1)"
-        info.Description = wordwrap(
-            "DVD Video Archiver is an application developed by the Texas Advanced Computing Center "
-            "to aid in the the preservation of digital art stored in DVD media.\n\n\n"
-            "\t\t\t\tPROGRAM USAGE\n\n"
+        msg = wordwrap(
             "Output Directory:\nType or Select the directory in which you wish to store all output files.\n\n"
             "Output File Name:\nProvide a name to be used for all generated files.\n\n"
-            "DVD Directory:\nType or Select the directory of the DVD or ISO image.\n\n"
+            "DVD Directory:\nType or Select the directory of the Master DVD or ISO image.\n\n"
             "Optional Attributes:\nSelect the preservation media files that you would like to be generated.\n\n"
             "Logging:\nAll application procedures will be documented in the program log window.\n\n",
+            500, wx.ClientDC(self.panel))
+        helpWindow = GMD.GenericMessageDialog(self, msg,"Help", wx.OK | wx.ICON_QUESTION)
+        helpWindow.ShowModal()
+        helpWindow.Destroy()
+        
+    def open_about(self, event):
+        info = wx.AboutDialogInfo()
+        info.Name = "DVD Video Archiver"
+        info.Version = "(version 1.0.0)"
+        info.Description = wordwrap(
+            "DVD Video Archiver is an application developed by the Texas Advanced Computing Center "
+            "to aid in the the preservation of digital art stored in DVD media.\n\n\n",
             350, wx.ClientDC(self.panel))
         info.WebSite = ("https://github.com/hnieto/DVD-Video-Archiver", "Github Repo")
         info.Developers = ["Heriberto Nieto", "Maria Esteva", "Karla Vega", "Summer Gunnels"]
+        info.License = wordwrap(
+            "This program is free software: you can redistribute it and/or modify"
+            "it under the terms of the GNU General Public License as published by"
+            "the Free Software Foundation\n\n"
+            "This program is distributed in the hope that it will be useful, "
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
+            "GNU General Public License for more details.\n\n"
+            "You should have received a copy of the GNU General Public License "
+            "along with this program.  If not, see <http://www.gnu.org/licenses/>.",
+            450, wx.ClientDC(self.panel))
         
         # Show the wx.AboutBox
         wx.AboutBox(info)
